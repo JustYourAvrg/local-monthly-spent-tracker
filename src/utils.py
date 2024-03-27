d609106310464d0d760421e3be70f16a7b1302f3
JustYourAvrg <130159201+JustYourAvrg@users.noreply.github.com>
2024-03-27 11:46:08 -0400

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+class Utility:
+    @staticmethod
+    def get_month_str(month: int) -> str:
+        months = {
+            1: 'January',
+            2: 'February',
+            3: 'March',
+            4: 'April',
+            5: 'May',
+            6: 'June',
+            7: 'July',
+            8: 'August',
+            9: 'September',
+            10: 'October',
+            11: 'November',
+            12: 'December'
+        }
+
+        return months[month]
